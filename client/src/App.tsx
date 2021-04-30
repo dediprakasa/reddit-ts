3044b7b9e2bc6dfb53c61c3970ef701b7ee29304
deprak <dedprakasa@gmail.com>
2021-04-30 22:56:37 +0700

    urql cache

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,8 +9,15 @@ import {
   IonTabs,
 } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
-import { Provider, createClient } from "urql";
+import { Provider, createClient, dedupExchange, fetchExchange } from "urql";
 import { ellipse, square, triangle } from "ionicons/icons";
+import { cacheExchange, QueryInput, Cache } from "@urql/exchange-graphcache";
+import {
+  MeDocument,
+  MeQuery,
+  LoginMutation,
+  RegisterMutation,
+} from "./generated/graphql";
 
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css";
@@ -34,11 +41,63 @@ import Register from "./pages/Register";
 import Tabs from "./Tabs";
 import Login from "./pages/Login";
 
+function betterUpdateQuery<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+}
+
 const client = createClient({
   url: "http://localhost:4000/graphql",
   fetchOptions: {
     credentials: "include",
   },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (_result, args, cache, info) => {
+            betterUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.login.user,
+                  };
+                }
+              }
+            );
+          },
+
+          register: (_result, args, cache, info) => {
+            betterUpdateQuery<RegisterMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.register.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.register.user,
+                  };
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
 
 const App: React.FC = () => (
